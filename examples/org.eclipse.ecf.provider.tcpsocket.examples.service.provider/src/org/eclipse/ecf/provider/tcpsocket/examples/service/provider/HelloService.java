159239b46315f000f0a973dcf51f8c7db0770893
Scott Lewis <scottslewis@gmail.com>
2023-08-01 11:20:04 -0700

    Added port to service properties in HelloService

--- examples/org.eclipse.ecf.provider.tcpsocket.examples.service.provider/src/org/eclipse/ecf/provider/tcpsocket/examples/service/provider/HelloService.java
+++ examples/org.eclipse.ecf.provider.tcpsocket.examples.service.provider/src/org/eclipse/ecf/provider/tcpsocket/examples/service/provider/HelloService.java
@@ -14,7 +14,8 @@ import org.eclipse.ecf.provider.tcpsocket.examples.service.api.IHello;
 import org.osgi.service.component.annotations.Component;
 
 @Component(immediate = true, property = { "my.id=myservice", "service.exported.interfaces=*",
-		"service.exported.configs=" + TCPSocketConstants.SERVER_PROVIDER_CONFIG_TYPE })
+		"service.exported.configs=" + TCPSocketConstants.SERVER_PROVIDER_CONFIG_TYPE,
+		TCPSocketConstants.SERVER_PROVIDER_CONFIG_TYPE+".port:Integer=5555"})
 public class HelloService implements IHello {
 
 	public HelloService() {
